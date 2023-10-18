f073bab8b89b774fc578e982ee6bd171931d74eb
mraden <143109899+mraden1@users.noreply.github.com>
2023-10-18 12:31:38 -0700

    first commit

---
+++ scripts/map.js
@@ -0,0 +1,9 @@
+var map = L.map('map', {
+  center: [33.8, -117.8],
+  zoom: 13
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
