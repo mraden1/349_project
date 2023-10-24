70a4f00288b7621e748b4997c27b871cdfca9efb
mraden <143109899+mraden1@users.noreply.github.com>
2023-10-23 18:43:50 -0700

    made it so a marker appears where the user clicks

--- scripts/clickhandler.js
+++ scripts/clickhandler.js
@@ -2,13 +2,15 @@
   'use strict';
   var App = window.App || {};
   var popup = L.popup();
-
+  var marker = L.marker();
+  
 
   function onMapClick(e) {
     popup
       .setLatLng(e.latlng)
       .setContent("You clicked the map at " + e.latlng)
       .openOn(map);
+    marker = L.marker(e.latlng).addTo(map);
   }
 
   map.on('click', onMapClick);
