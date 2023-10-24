284890acfde680da69f6f0a6b02f73153d0dda91
mraden <143109899+mraden1@users.noreply.github.com>
2023-10-23 21:06:18 -0700

    markers can be added and deleted and the information in their popups changed

--- scripts/clickhandler.js
+++ scripts/clickhandler.js
@@ -2,18 +2,32 @@
   'use strict';
   var App = window.App || {};
 
+  function editMarker() {
+    var newInput = prompt('Enter new information or leave blank to delete marker');
+    if (newInput) {
+      marker.getPopup().setContent(newInput);
+    } else {
+      marker.remove();
+    }
+  }
+
   function displayPopup() {
-    var userInput = prompt('where the party at?');
+    var userInput = prompt("Tell us where the party at");
     if (userInput) { 
       marker.bindPopup(userInput).openPopup();
+    } else {
+      marker.remove();
     }
+    marker.on('dblclick', editMarker);
+    return (userInput);
   }
 
   function onMapClick(e) {
     window.marker = L
       .marker(e.latlng)
       .addTo(map);
-    displayPopup();
+
+    return (displayPopup());
   }
   map.on('click', onMapClick);
   window.App = App;
