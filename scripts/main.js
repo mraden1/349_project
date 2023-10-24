d1169e32d227f8fea2bbc28ea07a53018552857f
mraden <143109899+mraden1@users.noreply.github.com>
2023-10-24 00:26:27 -0700

    fixed the issue with the popups and markers editing and deleting

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,39 @@
 (function (window) {
   'use strict';
   var App = window.App || {};
-  var onClick = map;
+  var map = L.map('map', {
+    center: [33.8823, -117.8851],
+    zoom: 16
+  });
 
-  addEventListener('click', onClick);
-  
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+
+  function newMarker(e) {
+    var userInput = prompt('enter the info');
+    if (userInput) {
+      var marker =  L.marker(e.latlng)
+                    .bindPopup(userInput)
+                    .addTo(map)
+                    .openPopup()
+                    .on('dblclick', editMarker);
+      return (marker);
+    }
+  }
+
+  function editMarker() {
+    var userInput = prompt('enter the info again');
+    if (userInput) {
+      this.getPopup().setContent(userInput);
+    } else { 
+      this.remove(); 
+    }
+  }
+
+  var group = L.featureGroup();
+  group.extend(map.on('click', newMarker));
+
   window.App = App;
 })(window);
--- scripts/map.js
+++
@@ -1,9 +0,0 @@
-var map = L.map('map', {
-  center: [33.8823, -117.8851],
-  zoom: 16
-});
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
