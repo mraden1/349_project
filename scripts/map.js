0e589caeabf905033209c1f7a0920e3c51eef6d3
mraden <143109899+mraden1@users.noreply.github.com>
2023-10-23 21:56:21 -0700

    finally got the title centered. the popup boxes and marker deleting isnt actually quite right. it just deletes the most recently added marker instead of the selected one

--- scripts/map.js
+++ scripts/map.js
@@ -4,6 +4,6 @@ var map = L.map('map', {
 });
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 16,
+    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
